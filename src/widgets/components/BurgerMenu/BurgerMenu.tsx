1c9b0e7123c5338149ff2adf9922f9c7c377eb55
EaleksS <ealgenv@gmail.com>
2023-05-18 01:58:38 +0900

    .

--- src/widgets/components/BurgerMenu/BurgerMenu.tsx
+++ src/widgets/components/BurgerMenu/BurgerMenu.tsx
@@ -3,11 +3,9 @@ import styles from "./BurgerMenu.module.scss";
 import { Modal, Nav } from "../../../entities";
 import { useBurgerMenu } from "../../store/burgerMenu.store";
 import { useCloseRedirect } from "../../../shared/lib/hooks/useCloseRedirect";
-import { useTranslation } from "react-i18next";
 
 export const BurgerMenu: FC = (): JSX.Element => {
   const { isActive, setIsActive } = useBurgerMenu();
-  const { t } = useTranslation();
 
   useCloseRedirect(setIsActive);
 
