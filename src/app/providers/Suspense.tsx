9dee873242d0a4897b95f7fda3aa493a550bdcc2
EaleksS <ealgenv@gmail.com>
2023-05-18 12:19:54 +0900

    loader

--- src/app/providers/Suspense.tsx
+++ src/app/providers/Suspense.tsx
@@ -1,9 +1,31 @@
-import { FC, ReactNode, Suspense } from "react";
+import { CSSProperties, FC, ReactNode, Suspense } from "react";
+import { Loader } from "../../shared";
 
 interface Props {
   children: ReactNode;
 }
 
 export const PSuspense: FC<Props> = ({ children }): JSX.Element => {
-  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+  const containerStyle: CSSProperties = {
+    position: "fixed",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  return (
+    <Suspense
+      fallback={
+        <div style={containerStyle}>
+          <Loader />
+        </div>
+      }
+    >
+      {children}
+    </Suspense>
+  );
 };
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,5 +3,6 @@ import { Text } from "./ui/Text/Text";
 import { Dots } from "./ui/Dots/Dots";
 import { useCloseBody } from "./lib/hooks/useCloseBody";
 import { useCopyToClipboard } from "./lib/hooks/useCopyToClipboard";
+import { Loader } from "./ui/Loader/Loader";
 
-export { Button, Text, Dots, useCloseBody, useCopyToClipboard };
+export { Button, Text, Dots, useCloseBody, useCopyToClipboard, Loader };
