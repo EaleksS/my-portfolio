0f4b3841b865e0ae9fc5784398c9ec147b8fda72
EaleksS <ealgenv@gmail.com>
2023-05-29 22:20:50 +0900

    .

--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { CompleteApps, Layout, SmallProjects } from "../../widgets";
+import { CompleteApps, Layout } from "../../widgets";
 import styles from "./Works.module.scss";
 import { Text } from "../../shared";
 import { useTranslation } from "react-i18next";
@@ -17,7 +17,7 @@ export const Works: FC = (): JSX.Element => {
         <Text>{t("List_of_my")}</Text>
       </div>
       <CompleteApps />
-      <SmallProjects />
+      {/* <SmallProjects /> */}
     </Layout>
   );
 };
